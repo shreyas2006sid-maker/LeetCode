f4247a249e2bab2121d6bc9972bb38f084a43814
shreyas2006sid-maker <shreyas2006sid@gmail.com>
2026-08-04 08:44:17 +0530

    Time: 9 ms (97.54%) | Memory: 59.3 MB (77.01%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null)return head;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode mid=slow.next;
+        slow.next=null;
+
+        ListNode left=sortList(head);
+        ListNode right=sortList(mid);
+
+        return merge(left,right);
+
+
+    }
+    public ListNode merge(ListNode left,ListNode right){
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+
+        while(left!=null && right!=null){
+            if(left.val<=right.val){
+                temp.next=left;
+                left=left.next;
+            }
+            else{
+                temp.next=right;
+                right=right.next;
+            }
+            temp=temp.next;
+        }
+        if(left!=null)temp.next=left;
+        if(right!=null)temp.next=right;
+        return dummy.next;
+    }
+}
