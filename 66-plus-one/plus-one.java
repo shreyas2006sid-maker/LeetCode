924bdbf211f7f6cb63d7abdb5abe8e2f7dd23136
shreyas2006sid-maker <shreyas2006sid@gmail.com>
2026-08-08 11:04:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (23.97%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] arr) {
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]<9){
+                arr[i]++;
+                return arr;
+            }
+            arr[i]=0;
+
+        }
+        int[] ans=new int[arr.length+1];
+        ans[0]=1;
+        return ans;
+        
+    }
+}
