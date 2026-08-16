7c239b882228b54675a9ba338ee93a0507e98a9d
shreyas2006sid-maker <shreyas2006sid@gmail.com>
2026-08-16 10:16:45 +0530

    Time: 4 ms (1.80%) | Memory: 94.4 MB (51.94%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,14 @@
-class Solution {
-    public int maxProfit(int[] prices) {
-        int buy=prices[0];
-        int p=0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]<buy){
-            buy=prices[i];
-            }
-            else if(prices[i]-buy>p){
-                p=prices[i]-buy;
+
+        class Solution {
+    public int maxProfit(int[] nums) {
+        int profit =0;
+        int min = nums[0];
+        for(int i =1;i< nums.length;i++){
+            min = Math.min(min,nums[i]);
+            if (nums[i]>nums[i-1]){
+                profit = Math.max(profit,nums[i]-min);
             }
         }
-        return p;
+        return profit;
     }
 }
