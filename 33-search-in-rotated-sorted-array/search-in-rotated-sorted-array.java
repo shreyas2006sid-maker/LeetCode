69380ca8d3ae5a1ea7e9f94d800ed07c14cbc41f
shreyas2006sid-maker <shreyas2006sid@gmail.com>
2026-08-30 10:09:09 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (28.16%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,25 +1,39 @@
 class Solution {
-    public int search(int[] arr, int target) {
-    
-        int l=0;
-        int r=arr.length-1;
-        int res=-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(arr[mid]==target){
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
                 return mid;
             }
-            else if(arr[l]<=arr[mid]){
-                if(target>=arr[l] && target<arr[mid])r=mid-1;
-                else l=mid+1;
+
+            // Left half is sorted
+            if (nums[left] <= nums[mid]) {
+
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
             }
-            else{
-                if(target>arr[mid] && target<=arr[r])l=mid+1;
-                else r=mid-1;
+
+            // Right half is sorted
+            else {
+
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
-           
         }
-         return -1;
+
+        return -1;
     }
-        
 }
